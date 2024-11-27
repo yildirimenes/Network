40fc112a3408ad0d784b8427e10ea6d1467f99dc
enes_yildirim <enes_yildirim@beymen.com>
2024-11-27 13:33:30 +0300

    implementation splash screen

--- app/src/main/java/group/beymen/network/ui/MainActivity.kt
+++ app/src/main/java/group/beymen/network/ui/MainActivity.kt
@@ -4,22 +4,33 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import dagger.hilt.android.AndroidEntryPoint
 import group.beymen.network.common.LanguageChangeHelper
 import group.beymen.network.ui.main.MainScreen
+import group.beymen.network.ui.main.MainViewModel
 import group.beymen.network.ui.theme.MyappTheme
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private val mainViewModel by viewModels<MainViewModel>()
     private val languageChangeHelper by lazy { LanguageChangeHelper() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        mainViewModel.loginCheck()
+        installSplashScreen().apply {
+            setKeepOnScreenCondition{
+                mainViewModel.isLoading.value
+            }
+        }
+
         setContent {
             val currentLanguageCode = languageChangeHelper.getLanguageCode(applicationContext)
             var currentLanguage by remember { mutableStateOf(currentLanguageCode) }
